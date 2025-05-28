5038f41baaa94d04443ad93e32b31f82ad6d2a8c
Himanshu2557-coder <himanshusainidtu@gmail.com>
2025-05-28 13:57:48 +0530

    done

---
+++ INTRO_Topics/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hi, world!");
+        System.out.println("This is a simple Java program.");
+        System.out.print("It prints a greeting message to the console.");
+        System.out.printf("Feel free to modify this code and run it again.");
+        System.out.print("Happy coding!");
+        System.out.printf("java is %d percent fun.",100);
+        
+    }
+}
